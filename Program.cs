c0e07c7c8fbbda9d43290fc59a5cf0f672f75ea9
Tommy Waalkes <Tommy@grandcircus.co>
2017-06-22 11:14:18 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("Tommy");
+            names.Add(1);
+            names.Add(true);
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            string n = names[0];
+            Console.WriteLine(n);
+                }
+    }
+}
